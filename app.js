fa1082ccc92ca8ae488b44101a7352a14cad1b43
devesh/webdunia <devesh.sant@webdunia.net>
2020-01-31 16:06:57 +0530

    clusters added

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const redis = require('redis');
 let redisClient = redis.createClient();
+//var RedisCluster = require('redis-cluster').clusterClient;
+var RedisCluster = require('redis-clustr');
+//var rediscl = RedisCluster;
+//var redisPubSub = RedisCluster;
+//var assert = require('assert');
 
 redisClient.on('connect', function(){
   console.log('Redis Connection Successful');
@@ -9,14 +14,14 @@ redisClient.on('connect', function(){
 //setvalue function 
 function setSetsvalues(Setskey,Setvalue){
 redisClient.sadd(Setskey ,Setvalue, function(err, reply) {
-    console.log(reply + " set(s) added !"); 
+   // console.log(reply + " set(s) added !"); 
 });
 
 }
 function getSetsvalue(Setskey){
 redisClient.SMEMBERS( Setskey ,function(err,data){
     if (err) throw err;
-    console.log(data);
+   /// console.log(data);
 })
 
 }
@@ -26,14 +31,14 @@ function sethashvalues(hashkey,hashvalue){
 redisClient.hmset(
              hashkey,
              hashvalue,function(err,reply){
-              console.log(reply + " hash(s) added !"); 
+              //console.log(reply + " hash(s) added !"); 
             });
 }
 
 function gethashvalues(hashkey){ 
 redisClient.hgetall( hashkey , function(err, object) {
   if (err) throw err;
-  console.log(object);
+  //console.log(object);
 });
 }
 
@@ -42,3 +47,36 @@ getSetsvalue('name');
 
 sethashvalues('nameaddress',{'abhishek1': 'indore','devesh1': 'indore'});
 gethashvalues('nameaddress');
+//redis clusters 
+// var cluster = [
+//   {name: 'redis01', link: '127.0.0.1:6379', slots: [   0, 5462], options: {max_attempts: 5}},
+//   {name: 'redis02', link: '127.0.0.1:7379', slots: [5463, 12742], options: {max_attempts: 5}},
+//   {name: 'redis03', link: '127.0.0.1:8379', slots: [12743, 16384], options: {max_attempts: 5}}
+// ];
+ 
+// var r = poorMansClusterClient(cluster);
+ 
+// r.set('foo', 'bar', function (err, reply) {
+//   if (err) throw err;
+//   assert.equal(reply,'OK');
+ 
+//   r.get('foo', function (err, reply) {
+//     if (err) throw err;
+//     assert.equal(reply, 'bar');
+//   });
+// });
+
+var rediscl = new RedisCluster({
+  servers : [
+    {name: 'redis01', link: '127.0.0.1:6379', slots: [   0, 5462], options: {max_attempts: 5}},
+    {name: 'redis02', link: '127.0.0.1:7379', slots: [5463, 12742], options: {max_attempts: 5}},
+     {name: 'redis03', link: '127.0.0.1:8379', slots: [12743, 16384], options: {max_attempts: 5}}
+    ],
+  createClient: function(port, host) {
+    console.log(port+host);
+    // this is the default behaviour
+    return redis.createClient(port, host);
+  }
+
+});
+console.log(rediscl);
